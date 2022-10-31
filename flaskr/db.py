cdeb8fa0f4e4b72d033cd8dfe929a9d9260190fb
andrewMasiero <andymasiero@gmail.com>
2022-10-31 09:06:18 -0500

    monday commits

--- flaskr/db.py
+++ flaskr/db.py
@@ -33,7 +33,7 @@ def init_db():
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo('Initialize the database.')
+    click.echo('Initialized')
 
 
 def init_app(app):
